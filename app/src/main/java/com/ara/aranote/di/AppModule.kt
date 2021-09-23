c4c1c9638584affd61da98f598194146a84ea52e
hossein hashemi <hashemi@tutamail.com>
2021-09-23 09:38:33 +0330

    Database DI Module added

--- app/src/main/java/com/ara/aranote/di/AppModule.kt
+++ app/src/main/java/com/ara/aranote/di/AppModule.kt
@@ -1,6 +1,13 @@
 package com.ara.aranote.di
 
 import android.content.Context
+import com.ara.aranote.data.local_data_source.NoteDao
+import com.ara.aranote.data.model.NoteModel
+import com.ara.aranote.data.repository.NoteRepositoryImpl
+import com.ara.aranote.data.util.DomainMapperImpl
+import com.ara.aranote.domain.entity.Note
+import com.ara.aranote.domain.repository.NoteRepository
+import com.ara.aranote.domain.util.DomainMapper
 import com.ara.aranote.ui.BaseApplication
 import dagger.Module
 import dagger.Provides
@@ -18,4 +25,15 @@ object AppModule {
     fun provideApplication(@ApplicationContext context: Context): BaseApplication {
         return context as BaseApplication
     }
+
+    @Singleton
+    @Provides
+    fun provideRepository(
+        noteDao: NoteDao,
+        domainMapper: DomainMapper<NoteModel, Note>,
+    ): NoteRepository = NoteRepositoryImpl(noteDao = noteDao, domainMapper = domainMapper)
+
+    @Singleton
+    @Provides
+    fun provideDomainMapper(): DomainMapper<NoteModel, Note> = DomainMapperImpl()
 }
---
+++ app/src/main/java/com/ara/aranote/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package com.ara.aranote.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ara.aranote.data.local_data_source.NoteDao
+import com.ara.aranote.data.local_data_source.NoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): NoteDatabase =
+        Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: NoteDatabase): NoteDao = database.getNoteDao()
+}
