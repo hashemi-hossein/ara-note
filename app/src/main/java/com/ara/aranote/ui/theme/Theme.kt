bd4208340a7cc17883fda56e7d4a38e19831808d
hossein hashemi <hashemi@tutamail.com>
2021-09-23 10:53:25 +0330

    AppTheme changed

--- app/src/main/java/com/ara/aranote/ui/theme/Theme.kt
+++ app/src/main/java/com/ara/aranote/ui/theme/Theme.kt
@@ -1,34 +1,35 @@
 package com.ara.aranote.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Green200,
+    primaryVariant = Green700,
+//    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = Green500,
+    primaryVariant = Green700,
+    secondary = Blue500,
+    secondaryVariant = Blue700,
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
+    onSecondary = Color.White,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
-fun AppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun AppTheme(
+    darkTheme: Boolean = false, // isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
