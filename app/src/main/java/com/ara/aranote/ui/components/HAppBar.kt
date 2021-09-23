29fcf5f6a6c44ce1bbe05a561c8f658d8bd6bc64
hossein hashemi <hashemi@tutamail.com>
2021-09-23 10:58:29 +0330

    custom AppBar HAppBar.kt added

---
+++ app/src/main/java/com/ara/aranote/ui/components/HAppBar.kt
@@ -0,0 +1,54 @@
+package com.ara.aranote.ui.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.ara.aranote.R
+
+enum class AppBarNavButtonType {
+    MENU, BACK
+}
+
+@Composable
+fun HAppBar(
+    title: String = stringResource(id = R.string.app_name),
+    appBarNavButtonType: AppBarNavButtonType = AppBarNavButtonType.MENU,
+    actions: @Composable RowScope.() -> Unit = {},
+    onNavButtonClick: () -> Unit,
+) {
+    TopAppBar(
+        elevation = 0.dp,
+        backgroundColor = Color.Transparent,
+        contentColor = MaterialTheme.colors.primary,
+        actions = actions,
+        title = {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavButtonClick) {
+                Icon(
+                    imageVector =
+                    if (appBarNavButtonType == AppBarNavButtonType.BACK) Icons.Filled.ArrowBack
+                    else Icons.Filled.Menu,
+                    contentDescription =
+                    if (appBarNavButtonType == AppBarNavButtonType.BACK) stringResource(R.string.cd_happbar_back)
+                    else stringResource(R.string.cd_happbar_menu),
+                )
+            }
+        },
+    )
+}
