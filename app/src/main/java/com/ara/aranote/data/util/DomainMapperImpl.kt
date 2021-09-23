d70593e2508b65434de1702c26cdc9b07d996017
hossein hashemi <hashemi@tutamail.com>
2021-09-23 09:22:16 +0330

    DomainMapperImpl.kt util added to data layer

---
+++ app/src/main/java/com/ara/aranote/data/util/DomainMapperImpl.kt
@@ -0,0 +1,34 @@
+package com.ara.aranote.data.util
+
+import com.ara.aranote.data.model.NoteModel
+import com.ara.aranote.domain.entity.Note
+import com.ara.aranote.domain.util.DomainMapper
+
+class DomainMapperImpl : DomainMapper<NoteModel, Note> {
+
+    override fun mapToDomainEntity(model: NoteModel): Note {
+        return Note(
+            id = model.id,
+            text = model.text,
+            addedDateTime = model.addedDateTime,
+            alarmDateTime = model.alarmDateTime,
+        )
+    }
+
+    override fun mapFromDomainEntity(domainEntity: Note): NoteModel {
+        return NoteModel(
+            id = domainEntity.id,
+            text = domainEntity.text,
+            addedDateTime = domainEntity.addedDateTime,
+            alarmDateTime = domainEntity.alarmDateTime,
+        )
+    }
+
+    override fun toDomainList(lstModel: List<NoteModel>): List<Note> {
+        return lstModel.map { mapToDomainEntity(it) }
+    }
+
+    override fun fromDomainList(lstDomainEntity: List<Note>): List<NoteModel> {
+        return lstDomainEntity.map { mapFromDomainEntity(it) }
+    }
+}
