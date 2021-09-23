4f5b68886a40cb23010799bd8ad1a9896ef5c3e1
hossein hashemi <hashemi@tutamail.com>
2021-09-23 09:07:31 +0330

    Hilt DI and Application class added

---
+++ app/src/main/java/com/ara/aranote/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.ara.aranote.di
+
+import android.content.Context
+import com.ara.aranote.ui.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext context: Context): BaseApplication {
+        return context as BaseApplication
+    }
+}
---
+++ app/src/main/java/com/ara/aranote/ui/BaseApplication.kt
@@ -0,0 +1,18 @@
+package com.ara.aranote.ui
+
+import android.app.Application
+import com.ara.aranote.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
