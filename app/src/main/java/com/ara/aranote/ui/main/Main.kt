0c433994d2f3f24e4151375d40a47cffd71ebac4
hossein hashemi <hashemi@tutamail.com>
2021-09-23 11:51:50 +0330

    HomeScreen and NoteDetailScreen added to navigation compose in Main.kt

--- app/src/main/java/com/ara/aranote/ui/main/Main.kt
+++ app/src/main/java/com/ara/aranote/ui/main/Main.kt
@@ -1,9 +1,19 @@
 package com.ara.aranote.ui.main
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.navArgument
+import com.ara.aranote.domain.viewmodels.HomeViewModel
+import com.ara.aranote.domain.viewmodels.NoteDetailViewModel
+import com.ara.aranote.ui.screens.HomeScreen
+import com.ara.aranote.ui.screens.NoteDetailScreen
+import com.ara.aranote.util.INVALID_NOTE_ID
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -14,14 +24,37 @@ fun AppMain(
     navController: NavHostController = rememberAnimatedNavController(),
     startDestination: String = NavScreen.Home.route,
 ) {
+    BoxWithConstraints {
+        AnimatedNavHost(
+            navController = navController,
+            startDestination = startDestination,
+        ) {
 
-    AnimatedNavHost(
-        navController = navController,
-        startDestination = startDestination,
-    ) {
+            composable(NavScreen.Home.route) {
+                val viewModel = hiltViewModel<HomeViewModel>()
+                HomeScreen(
+                    viewModel = viewModel,
+                ) {
+                    navController.navigate(NavScreen.NoteDetail.route + "/" + it)
+                }
+            }
 
-        composable(NavScreen.Home.route) {
-            Text(text = "")
+            composable(
+                NavScreen.NoteDetail.route + "/{id}",
+                arguments = listOf(navArgument("id") { type = NavType.IntType }),
+            ) { backStackEntry: NavBackStackEntry ->
+                val id = backStackEntry.arguments?.getInt("id") ?: INVALID_NOTE_ID
+                val viewModel = hiltViewModel<NoteDetailViewModel>()
+                LaunchedEffect(true) {
+                    viewModel.prepareNote(id)
+                }
+                NoteDetailScreen(
+                    viewModel = viewModel,
+                    id = id,
+                ) {
+                    navController.navigateUp()
+                }
+            }
         }
     }
 }
@@ -29,4 +62,6 @@ fun AppMain(
 sealed class NavScreen(val route: String) {
 
     object Home : NavScreen("Home")
+
+    object NoteDetail : NavScreen("NoteDetail")
 }
