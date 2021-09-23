57decb38a9d97b5f223143d5568f040050a535cf
hossein hashemi <hashemi@tutamail.com>
2021-09-23 10:39:15 +0330

    FakeNoteRepository.kt added in order to testing viewmodels

---
+++ app/src/test/java/com/ara/aranote/domain/repository/FakeNoteRepository.kt
@@ -0,0 +1,51 @@
+package com.ara.aranote.domain.repository
+
+import com.ara.aranote.domain.entity.Note
+import com.ara.aranote.util.INVALID_NOTE_ID
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.update
+
+class FakeNoteRepository : NoteRepository {
+
+    private val notes = mutableMapOf<Int, Note>()
+    private val notesFlow = MutableStateFlow(notes.values.toList())
+
+    override suspend fun insertNote(note: Note): Int {
+        val r = if (notes.put(note.id, note) == null) note.id else INVALID_NOTE_ID
+        notesFlow.update { notes.values.toList() }
+        return r
+    }
+
+    override fun observeNotes(): Flow<List<Note>> {
+        return flow {
+            notesFlow.collect { emit(it) }
+        }
+    }
+
+    override suspend fun getNote(id: Int): Note? {
+        return notes[id]
+    }
+
+    override suspend fun updateNote(note: Note): Boolean {
+        val r = notes.put(note.id, note) == null
+        notesFlow.update { notes.values.toList() }
+        return r
+    }
+
+    override suspend fun deleteNote(note: Note): Boolean {
+        val r = notes.remove(note.id) != null
+        notesFlow.update { notes.values.toList() }
+        return r
+    }
+
+    override suspend fun getLastId(): Int {
+        return if (notes.keys.isEmpty()) 0 else notes.keys.last()
+    }
+
+    override suspend fun getAllNotesWithAlarm(): List<Note> {
+        return notes.values.filter { it.alarmDateTime != null }
+    }
+}
