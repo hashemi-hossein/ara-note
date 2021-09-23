f9cbf8114fff0784b5f03b537fcbd0de5cac912c
hossein hashemi <hashemi@tutamail.com>
2021-09-23 10:42:54 +0330

    HomeViewModelTest.kt added

---
+++ app/src/test/java/com/ara/aranote/domain/viewmodels/HomeViewModelTest.kt
@@ -0,0 +1,40 @@
+package com.ara.aranote.domain.viewmodels
+
+import com.ara.aranote.domain.repository.FakeNoteRepository
+import com.ara.aranote.domain.repository.NoteRepository
+import com.ara.aranote.test_util.TestCoroutineRule
+import com.ara.aranote.test_util.TestUtil
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class HomeViewModelTest {
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    private lateinit var repository: NoteRepository
+    private lateinit var systemUnderTest: HomeViewModel
+
+    @Before
+    fun setUp() {
+        repository = FakeNoteRepository()
+        systemUnderTest = HomeViewModel(repository)
+    }
+
+    @Test
+    fun observeNotes() = runBlockingTest {
+        // arrange
+        repository.insertNote(TestUtil.tEntity)
+
+        // act
+        val r = systemUnderTest.notes.value
+
+        // assert
+        assertThat(r).containsExactly(TestUtil.tEntity).inOrder()
+    }
+}
