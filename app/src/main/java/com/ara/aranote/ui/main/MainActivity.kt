0843ccac91616ffa10c2f05998712410a28c8670
hossein hashemi <hashemi@tutamail.com>
2021-09-23 07:56:24 +0330

    move MainActivity.kt to ui.main package Clean up MainActivity.kt

---
+++ app/src/main/java/com/ara/aranote/ui/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ara.aranote.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ara.aranote.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+            }
+        }
+    }
+}
