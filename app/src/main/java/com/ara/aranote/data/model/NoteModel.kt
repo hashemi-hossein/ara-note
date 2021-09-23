e75b4ee6269cbe2fd8f6ecc9350a0eb72966cbbf
hossein hashemi <hashemi@tutamail.com>
2021-09-23 09:20:27 +0330

    NoteModel.kt database model added to data layer

---
+++ app/src/main/java/com/ara/aranote/data/model/NoteModel.kt
@@ -0,0 +1,19 @@
+package com.ara.aranote.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.datetime.LocalDateTime
+
+@Entity(tableName = "tblNotes")
+data class NoteModel(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
+    val id: Int,
+    @ColumnInfo(name = "text")
+    val text: String,
+    @ColumnInfo(name = "added_datetime")
+    val addedDateTime: LocalDateTime,
+    @ColumnInfo(name = "alarm_datetime")
+    val alarmDateTime: LocalDateTime? = null,
+)
