79cdeb0aeb94055110a1088da280b2fb5485e728
hossein hashemi <hashemi@tutamail.com>
2021-09-26 16:38:27 +0330

    HomeScreen.kt: showing all notebooks in drawer dialog for adding notebook have been added

--- app/src/main/java/com/ara/aranote/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/ara/aranote/ui/screens/HomeScreen.kt
@@ -1,26 +1,43 @@
 package com.ara.aranote.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ara.aranote.R
 import com.ara.aranote.domain.entity.Note
 import com.ara.aranote.domain.entity.Notebook
@@ -44,7 +61,8 @@ fun HomeScreen(
     HomeScreen(
         notes = notes,
         notebooks = notebooks,
-        navigateToNoteDetailScreen = navigateToNoteDetailScreen
+        navigateToNoteDetailScreen = navigateToNoteDetailScreen,
+        addNotebook = viewModel::addNotebook,
     )
 }
 
@@ -53,7 +71,11 @@ internal fun HomeScreen(
     notes: List<Note>,
     notebooks: List<Notebook>,
     navigateToNoteDetailScreen: (Int) -> Unit,
+    addNotebook: (String) -> Unit = {},
 ) {
+    var isDialogVisible by remember { mutableStateOf(false) }
+    val setDialogVisibility: (Boolean) -> Unit = { isDialogVisible = it }
+
     Scaffold(
         topBar = {
             HAppBar {
@@ -61,7 +83,8 @@ internal fun HomeScreen(
         },
         drawerContent = {
             HDrawer(
-                notebooks = notebooks
+                notebooks = notebooks,
+                setDialogVisibility = setDialogVisibility,
             )
         },
         floatingActionButton = {
@@ -77,6 +100,11 @@ internal fun HomeScreen(
             innerPadding = innerPadding,
             notes = notes,
             navigateToNoteDetailScreen = navigateToNoteDetailScreen,
+        )
+        HDialog(
+            isDialogVisible = isDialogVisible,
+            setDialogVisibility = setDialogVisibility,
+            addNotebook = addNotebook,
         )
     }
 }
@@ -108,14 +136,75 @@ private fun HBody(
 @Composable
 private fun HDrawer(
     notebooks: List<Notebook>,
+    setDialogVisibility: (Boolean) -> Unit,
 ) {
-    Column(modifier = Modifier) {
-        Text(text = stringResource(R.string.notebooks))
-        LazyColumn() {
-            itemsIndexed(notebooks) { index: Int, item: Notebook ->
-                Text(text = item.name)
+    Column(
+        modifier = Modifier.padding(horizontal = 10.dp, vertical = 15.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.notebooks),
+                style = MaterialTheme.typography.h6
+            )
+            IconButton(onClick = { setDialogVisibility(true) }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
         }
+        LazyColumn() {
+            items(notebooks) { item: Notebook ->
+                TextButton(
+                    onClick = { /*TODO*/ },
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.Start,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = item.name, style = MaterialTheme.typography.body1)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HDialog(
+    isDialogVisible: Boolean,
+    setDialogVisibility: (Boolean) -> Unit,
+    addNotebook: (String) -> Unit,
+) {
+    if (isDialogVisible) {
+        var text by rememberSaveable { mutableStateOf("") }
+        AlertDialog(
+            onDismissRequest = { setDialogVisibility(false) },
+            confirmButton = {
+                IconButton(onClick = {
+                    setDialogVisibility(false)
+                    addNotebook(text)
+                }) {
+                    Icon(imageVector = Icons.Default.Done, contentDescription = null)
+                }
+            },
+            text = {
+                Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                    Text(
+                        text = "Add Notebook",
+                        style = MaterialTheme.typography.body1,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    TextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        singleLine = true,
+                        label = { Text("name") },
+                    )
+                }
+            },
+        )
     }
 }
 
