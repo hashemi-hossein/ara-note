480e20bbd18ba3362c8092f61ccf8bad3b9d7561
hossein hashemi <hashemi@tutamail.com>
2021-10-11 20:59:13 +0330

    adding serialization plugin and making domain entities serializable

--- app/src/main/java/com/ara/aranote/domain/entity/Note.kt
+++ app/src/main/java/com/ara/aranote/domain/entity/Note.kt
@@ -1,7 +1,9 @@
 package com.ara.aranote.domain.entity
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Note(
     val id: Int,
     val notebookId: Int,
--- app/src/main/java/com/ara/aranote/domain/entity/Notebook.kt
+++ app/src/main/java/com/ara/aranote/domain/entity/Notebook.kt
@@ -1,5 +1,8 @@
 package com.ara.aranote.domain.entity
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Notebook(
     val id: Int,
     val name: String,
