b62a591fab4c98c078cb8ba9fc191f67a0e624ae
Hossein Hashemi <hashemi@tutamail.com>
2022-03-26 17:13:47 +0430

    updating TestCoroutineRule.kt to use latest api for coroutine testing

--- app/src/test_common/java/com/ara/aranote/test_util/TestCoroutineRule.kt
+++ app/src/test_common/java/com/ara/aranote/test_util/TestCoroutineRule.kt
@@ -1,28 +1,30 @@
 package com.ara.aranote.test_util
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
 class TestCoroutineRule(
-    private val dispatcher: CoroutineDispatcher = TestCoroutineDispatcher()
-) : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
 
     override fun starting(description: Description?) {
         super.starting(description)
-        Dispatchers.setMain(dispatcher)
+        Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description?) {
         super.finished(description)
-        cleanupTestCoroutines()
         Dispatchers.resetMain()
     }
+
+    fun runTestWithTestDispatcher(block: suspend () -> Unit) =
+        runTest(testDispatcher) { block() }
 }
