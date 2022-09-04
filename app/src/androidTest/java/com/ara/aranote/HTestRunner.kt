1f92e3489653b062b72ca541bae9974eae2c36d0
Hossein Hashemi <hashemi@tutamail.com>
2022-09-04 15:52:21 +0430

    add HTestRunner.kt

---
+++ app/src/androidTest/java/com/ara/aranote/HTestRunner.kt
@@ -0,0 +1,13 @@
+package com.ara.aranote
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
