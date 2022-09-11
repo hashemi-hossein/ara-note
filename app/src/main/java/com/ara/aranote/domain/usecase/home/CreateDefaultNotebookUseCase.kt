0d1ac2d3955b6308a3f7cfa4aa64f80433dfe273
Hossein Hashemi <hashemi@tutamail.com>
2022-09-11 16:46:45 +0430

    modify CreateDefaultNotebookUseCase.kt to use UserPreferencesRepository.kt

--- app/src/main/java/com/ara/aranote/domain/usecase/home/CreateDefaultNotebookUseCase.kt
+++ app/src/main/java/com/ara/aranote/domain/usecase/home/CreateDefaultNotebookUseCase.kt
@@ -1,6 +1,7 @@
 package com.ara.aranote.domain.usecase.home
 
-import com.ara.aranote.data.datastore.AppDataStore
+import com.ara.aranote.data.datastore.UserPreferences
+import com.ara.aranote.data.repository.UserPreferencesRepository
 import com.ara.aranote.domain.entity.Notebook
 import com.ara.aranote.domain.repository.NotebookRepository
 import com.ara.aranote.util.DEFAULT_NOTEBOOK_ID
@@ -9,11 +10,11 @@ import javax.inject.Inject
 
 class CreateDefaultNotebookUseCase @Inject constructor(
     private val notebookRepository: NotebookRepository,
-    private val appDataStore: AppDataStore,
+    private val userPreferencesRepository: UserPreferencesRepository,
 ) {
     suspend operator fun invoke() {
-        if (!appDataStore.readPref(AppDataStore.DEFAULT_NOTEBOOK_EXISTENCE_KEY, false)) {
-            appDataStore.writePref(AppDataStore.DEFAULT_NOTEBOOK_EXISTENCE_KEY, true)
+        if (!userPreferencesRepository.read().doesDefaultNotebookExist) {
+            userPreferencesRepository.write(UserPreferences::doesDefaultNotebookExist, true)
             notebookRepository.insert(
                 Notebook(
                     id = DEFAULT_NOTEBOOK_ID,
