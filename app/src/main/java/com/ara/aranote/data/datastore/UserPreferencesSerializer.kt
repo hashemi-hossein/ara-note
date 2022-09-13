88c3d032a04ab7170af7194df5339dfe557a29a4
Hossein Hashemi <hashemi@tutamail.com>
2022-09-13 11:16:24 +0430

    UserPreferencesSerializer.kt: inject CoroutineDispatchers and change coroutine context

--- app/src/main/java/com/ara/aranote/data/datastore/UserPreferencesSerializer.kt
+++ app/src/main/java/com/ara/aranote/data/datastore/UserPreferencesSerializer.kt
@@ -1,34 +1,44 @@
 package com.ara.aranote.data.datastore
 
 import androidx.datastore.core.Serializer
+import com.ara.aranote.util.CoroutineDispatcherProvider
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import java.io.InputStream
 import java.io.OutputStream
 
-object UserPreferencesSerializer : Serializer<UserPreferences> {
+class UserPreferencesSerializer(
+    private val dispatcherProvider: CoroutineDispatcherProvider
+) : Serializer<UserPreferences> {
 
     override val defaultValue: UserPreferences
         get() = UserPreferences()
 
     override suspend fun readFrom(input: InputStream): UserPreferences {
-        return try {
-            Json.decodeFromString(
-                deserializer = UserPreferences.serializer(),
-                string = input.readBytes().decodeToString()
-            )
-        } catch (e: SerializationException) {
-            e.printStackTrace()
-            defaultValue
+        return withContext(dispatcherProvider.io + SupervisorJob()) {
+            try {
+                Json.decodeFromString(
+                    deserializer = UserPreferences.serializer(),
+                    string = input.readBytes().decodeToString()
+                )
+            } catch (e: SerializationException) {
+                e.printStackTrace()
+                defaultValue
+            }
         }
     }
 
+    @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
-        output.write(
-            Json.encodeToString(
-                serializer = UserPreferences.serializer(),
-                value = t
-            ).encodeToByteArray()
-        )
+        withContext(dispatcherProvider.io + SupervisorJob()) {
+            output.write(
+                Json.encodeToString(
+                    serializer = UserPreferences.serializer(),
+                    value = t
+                ).encodeToByteArray()
+            )
+        }
     }
 }
