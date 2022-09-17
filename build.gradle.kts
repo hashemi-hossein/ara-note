4ccdb4e937443266f9248be3fe504122bd5ffdd3
Hossein Hashemi <hashemi@tutamail.com>
2022-09-17 14:17:57 +0430

    add gradle task to do not update non stable updates upon stable ones for dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,25 @@ versionCatalogUpdate {
     }
 }
 
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
+
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+}
+
 subprojects {
     pluginManager.apply("com.diffplug.spotless")
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
