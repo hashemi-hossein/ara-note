0345426f5afd762db81bf177c1acbc3ea438dbd0
Hossein Hashemi <hashemi@tutamail.com>
2022-09-18 08:52:17 +0430

    HSnackBar.kt: add HSnackbarHost Composable

--- app/src/main/kotlin/com/ara/aranote/ui/component/HSnackBar.kt
+++ app/src/main/kotlin/com/ara/aranote/ui/component/HSnackBar.kt
@@ -1,7 +1,17 @@
 package com.ara.aranote.ui.component
 
-import androidx.compose.material.SnackbarHostState
-import androidx.compose.material.SnackbarResult
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -34,4 +44,24 @@ fun showSnackbar(
         snackbarJob.cancel()
     }
     snackbarFun()
+}
+
+/**
+ * Provide [SnackbarHost] which is configured for insets and large screens
+ */
+@Composable
+fun HSnackbarHost(
+    hostState: SnackbarHostState,
+    snackbar: @Composable (SnackbarData) -> Unit = { Snackbar(it) },
+    modifier: Modifier = Modifier,
+) {
+    SnackbarHost(
+        hostState = hostState,
+        snackbar = snackbar,
+        modifier = modifier
+            .systemBarsPadding()
+            // Limit the Snackbar width for large screens
+            .wrapContentWidth(align = Alignment.Start)
+            .widthIn(max = 550.dp),
+    )
 }
