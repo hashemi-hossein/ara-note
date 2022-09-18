b968273272bf9ec0d6fe47db7b54627f0218884c
Hossein Hashemi <hashemi@tutamail.com>
2022-09-18 09:01:35 +0430

    SettingsScreen.kt: Migrate to Material3

--- app/src/main/kotlin/com/ara/aranote/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/kotlin/com/ara/aranote/ui/screen/settings/SettingsScreen.kt
@@ -13,21 +13,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.ListItem
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Switch
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.OpenInNewOff
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,6 +41,7 @@ import com.afollestad.materialdialogs.color.colorChooser
 import com.ara.aranote.R
 import com.ara.aranote.data.datastore.UserPreferences
 import com.ara.aranote.ui.component.HAppBar
+import com.ara.aranote.ui.component.HSnackbarHost
 import com.ara.aranote.ui.component.showSnackbar
 import kotlinx.coroutines.CoroutineScope
 
@@ -73,7 +74,7 @@ fun SettingsScreen(
 }
 
 @SuppressLint("CheckResult")
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SettingsScreen(
     navigateUp: () -> Unit,
@@ -84,128 +85,143 @@ internal fun SettingsScreen(
     setIsDoubleBackToExitMode: (Boolean) -> Unit,
     exportData: (Uri, () -> Unit) -> Unit,
     importData: (Uri, () -> Unit) -> Unit,
-    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
     scope: CoroutineScope = rememberCoroutineScope(),
     context: Context = LocalContext.current,
 ) {
     Scaffold(
-        scaffoldState = scaffoldState,
-        topBar = {
-            HAppBar(title = "Settings", onNavButtonClick = navigateUp)
-        },
+        snackbarHost = { HSnackbarHost(hostState = snackbarHostState) },
+        topBar = { HAppBar(title = "Settings", onNavButtonClick = navigateUp) },
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            ListItem(trailing = {
-                Switch(checked = userPreferences.isDark, onCheckedChange = setIsDark)
-            }) {
-                Text(text = "Dark Theme")
-            }
-            ListItem(trailing = {
-                Switch(checked = userPreferences.isAutoSaveMode, onCheckedChange = setIsAutoSaveMode)
-            }) {
-                Text(text = "Auto save Mode")
-            }
-            ListItem(trailing = {
-                Switch(
-                    checked = userPreferences.isDoubleBackToExitMode,
-                    onCheckedChange = setIsDoubleBackToExitMode
-                )
-            }) {
-                Text(text = "Double back to exit Mode")
-            }
-            ListItem(trailing = {
-                Box(
-                    modifier = Modifier
-                        .size(35.dp)
-                        .clip(CircleShape)
-                        .background(color = Color(userPreferences.noteColor))
-                        .clickable {
-                            val colors = intArrayOf(
-                                android.graphics.Color.parseColor("#FF5722"),
-                                android.graphics.Color.parseColor("#d50000"),
-                                android.graphics.Color.parseColor("#c51162"),
-                                android.graphics.Color.parseColor("#aa00ff"),
-                                android.graphics.Color.parseColor("#6200ea"),
-                                android.graphics.Color.parseColor("#304ffe"),
-                                android.graphics.Color.parseColor("#2962ff"),
-                                android.graphics.Color.parseColor("#00796b"),
-                                android.graphics.Color.parseColor("#2e7d32"),
-                                android.graphics.Color.parseColor("#33691e"),
-                                android.graphics.Color.parseColor("#e65100"),
-                                android.graphics.Color.parseColor("#bf360c"),
-                                android.graphics.Color.parseColor("#8d6e63"),
-                                android.graphics.Color.parseColor("#546e7a"),
-                            )
-                            MaterialDialog(context).show {
-                                colorChooser(
-                                    colors,
-                                    initialSelection = userPreferences.noteColor.toInt(),
-                                    allowCustomArgb = true,
-                                ) { _, color ->
-//                                println("color=$color")
-                                    setNoteColor(color.toLong())
-                                }
-                                positiveButton(R.string.select)
-                                negativeButton(R.string.cancel)
+            ListItem(
+                headlineText = { Text(text = "Dark Theme") },
+                trailingContent = {
+                    Switch(checked = userPreferences.isDark, onCheckedChange = setIsDark)
+                },
+            )
+            ListItem(
+                headlineText = { Text(text = "Auto save Mode") },
+                trailingContent = {
+                    Switch(checked = userPreferences.isAutoSaveMode,
+                        onCheckedChange = setIsAutoSaveMode)
+                },
+            )
+            ListItem(
+                headlineText = { Text(text = "Double back to exit Mode") },
+                trailingContent = {
+                    Switch(
+                        checked = userPreferences.isDoubleBackToExitMode,
+                        onCheckedChange = setIsDoubleBackToExitMode
+                    )
+                },
+            )
+            ListItem(
+                headlineText = { Text(text = "Note Color") },
+                trailingContent = {
+                    Box(
+                        modifier = Modifier
+                            .size(35.dp)
+                            .clip(CircleShape)
+                            .background(color = Color(userPreferences.noteColor))
+                            .clickable {
+                                showMaterialDialog(context, userPreferences, setNoteColor)
                             }
-                        }
-                )
-            }) {
-                Text(text = "Note Color")
-            }
+                    )
+                },
+            )
             Divider()
+
             val activityResultLauncherCreateDocument =
                 rememberLauncherForActivityResult(ActivityResultContracts.CreateDocument()) {
                     if (it != null)
                         exportData(it) {
                             showSnackbar(
                                 scope = scope,
-                                snackbarHostState = scaffoldState.snackbarHostState,
+                                snackbarHostState = snackbarHostState,
                                 message = "Operation was done",
                                 actionLabel = "OK"
                             )
                         }
                 }
-            ListItem(trailing = {
-                Button(onClick = {
-                    activityResultLauncherCreateDocument.launch("AraNote.txt")
-                }) {
-                    Icon(
-                        imageVector = Icons.Default.OpenInNewOff,
-                        contentDescription = "Export Data"
-                    )
-                }
-            }) {
-                Text(text = "Export Data")
-            }
+            ListItem(
+                headlineText = { Text(text = "Export Data") },
+                trailingContent = {
+                    Button(onClick = {
+                        activityResultLauncherCreateDocument.launch("AraNote.txt")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.OpenInNewOff,
+                            contentDescription = "Export Data"
+                        )
+                    }
+                },
+            )
+
             val activityResultLauncherOpenDocument =
                 rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) {
                     if (it != null)
                         importData(it) {
                             showSnackbar(
                                 scope = scope,
-                                snackbarHostState = scaffoldState.snackbarHostState,
+                                snackbarHostState = snackbarHostState,
                                 message = "Operation was done",
                                 actionLabel = "OK"
                             )
                         }
                 }
-            ListItem(trailing = {
-                Button(onClick = {
-                    activityResultLauncherOpenDocument.launch(arrayOf("text/plain"))
-                }) {
-                    Icon(
-                        imageVector = Icons.Default.ExitToApp,
-                        contentDescription = "Import Data"
-                    )
-                }
-            }) {
-                Text(text = "Import Data")
-            }
+            ListItem(
+                headlineText = { Text(text = "Import Data") },
+                trailingContent = {
+                    Button(onClick = {
+                        activityResultLauncherOpenDocument.launch(arrayOf("text/plain"))
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ExitToApp,
+                            contentDescription = "Import Data"
+                        )
+                    }
+                },
+            )
         }
+    }
+}
+
+private fun showMaterialDialog(
+    context: Context,
+    userPreferences: UserPreferences,
+    setNoteColor: (Long) -> Unit,
+) {
+    val colors = intArrayOf(
+        android.graphics.Color.parseColor("#FF5722"),
+        android.graphics.Color.parseColor("#d50000"),
+        android.graphics.Color.parseColor("#c51162"),
+        android.graphics.Color.parseColor("#aa00ff"),
+        android.graphics.Color.parseColor("#6200ea"),
+        android.graphics.Color.parseColor("#304ffe"),
+        android.graphics.Color.parseColor("#2962ff"),
+        android.graphics.Color.parseColor("#00796b"),
+        android.graphics.Color.parseColor("#2e7d32"),
+        android.graphics.Color.parseColor("#33691e"),
+        android.graphics.Color.parseColor("#e65100"),
+        android.graphics.Color.parseColor("#bf360c"),
+        android.graphics.Color.parseColor("#8d6e63"),
+        android.graphics.Color.parseColor("#546e7a"),
+    )
+    MaterialDialog(context).show {
+        colorChooser(
+            colors,
+            initialSelection = userPreferences.noteColor.toInt(),
+            allowCustomArgb = true,
+        ) { _, color ->
+//                                println("color=$color")
+            setNoteColor(color.toLong())
+        }
+        positiveButton(R.string.select)
+        negativeButton(R.string.cancel)
     }
 }
