f7477d70b7221caf6538e30959a3d5733e3f709b
Hossein Hashemi <hashemi@tutamail.com>
2022-09-18 09:42:02 +0430

    NoteCard.kt: Migrate to Material3

--- app/src/main/kotlin/com/ara/aranote/ui/component/NoteCard.kt
+++ app/src/main/kotlin/com/ara/aranote/ui/component/NoteCard.kt
@@ -7,21 +7,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Card
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.Icon
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Alarm
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.dp
 import com.ara.aranote.domain.entity.Note
@@ -44,46 +40,41 @@ fun NoteCard(
             )
             .fillMaxWidth()
             .clickable(onClick = onClick),
-        elevation = 4.dp,
     ) {
-        val contentColor = if (Color(noteColor).luminance() > 0.5f) Color.Black else Color.White
-        Surface(
-            contentColor = contentColor,
-            color = Color(noteColor),
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
         ) {
-            Column(
-                verticalArrangement = Arrangement.SpaceBetween,
+            Text(
+                text = note.text,
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    textDirection = TextDirection.Content,
+                ),
+                maxLines = 10,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(7.dp)
+            )
+            CompositionLocalProvider(
+                LocalContentColor provides MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.38f)
             ) {
-                Text(
-                    text = note.text,
-                    style = MaterialTheme.typography.body2.copy(
-                        textDirection = TextDirection.Content,
-                    ),
-                    maxLines = 10,
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(7.dp)
-                )
-                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(3.dp)
-                            .padding(top = 5.dp),
-                    ) {
-                        Text(
-                            text = HDateTime.gerPrettyDateTime(note.addedDateTime),
-                            style = MaterialTheme.typography.caption,
+                        .padding(3.dp)
+                        .padding(top = 5.dp),
+                ) {
+                    Text(
+                        text = HDateTime.gerPrettyDateTime(note.addedDateTime),
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                    if (note.alarmDateTime != null)
+                        Icon(
+                            imageVector = Icons.Default.Alarm,
+                            contentDescription = null,
+                            modifier = Modifier.size(20.dp)
                         )
-                        if (note.alarmDateTime != null)
-                            Icon(
-                                imageVector = Icons.Default.Alarm,
-                                contentDescription = null,
-                                modifier = Modifier.size(20.dp)
-                            )
-                    }
                 }
             }
         }
