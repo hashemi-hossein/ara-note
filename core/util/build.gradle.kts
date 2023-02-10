88b42481f06c3978100b886714c37952d5f46446
Hossein Hashemi <hashemi@tutamail.com>
2023-02-10 19:47:13 +0330

    use LibraryPlugin in :core:util module

--- core/util/build.gradle.kts
+++ core/util/build.gradle.kts
@@ -1,27 +1,9 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    id("ara.library")
 }
 
 android {
     namespace = "aranote.core.util"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        buildConfig = false
-    }
 }
 
 dependencies {
