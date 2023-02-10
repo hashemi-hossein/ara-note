4ce14b1f8030be1b2071f3526ee3d38d00fa6e71
Hossein Hashemi <hashemi@tutamail.com>
2023-02-10 19:48:38 +0330

    use LibraryPlugin in :core_test module

--- core_test/build.gradle.kts
+++ core_test/build.gradle.kts
@@ -1,24 +1,9 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    id("ara.library")
 }
 
 android {
     namespace = "com.ara.core_test"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-    }
-    
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
