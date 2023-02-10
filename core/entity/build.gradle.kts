d8c81586a024f88bcc623899ab5bdfa83b2a5181
Hossein Hashemi <hashemi@tutamail.com>
2023-02-10 19:47:00 +0330

    use LibraryPlugin in :core:entity module

--- core/entity/build.gradle.kts
+++ core/entity/build.gradle.kts
@@ -1,28 +1,10 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    id("ara.library")
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
 }
 
 android {
     namespace = "aranote.core.entity"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-    }
-    
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        buildConfig = false
-    }
 }
 
 dependencies {
