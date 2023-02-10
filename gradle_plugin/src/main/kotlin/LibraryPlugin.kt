73b5eea21d3354dfe188e7468faf7cdbbd6a051c
Hossein Hashemi <hashemi@tutamail.com>
2023-02-10 19:51:28 +0330

    upgrade java version to 11

--- gradle_plugin/src/main/kotlin/LibraryPlugin.kt
+++ gradle_plugin/src/main/kotlin/LibraryPlugin.kt
@@ -22,11 +22,11 @@ class LibraryPlugin : Plugin<Project> {
                 }
                 
                 compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_1_8
-                    targetCompatibility = JavaVersion.VERSION_1_8
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
                 }
                 (this as ExtensionAware).extensions.configure<KotlinJvmOptions>("kotlinOptions") {
-                    jvmTarget = JavaVersion.VERSION_1_8.toString()
+                    jvmTarget = JavaVersion.VERSION_11.toString()
                 }
             }
         }
