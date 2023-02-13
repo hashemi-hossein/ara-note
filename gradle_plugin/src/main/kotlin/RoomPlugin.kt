827eacb246b9ebd91b5f76a7ac0efb5565e2b134
Hossein Hashemi <hashemi@tutamail.com>
2023-02-13 19:11:51 +0330

    add room schema location to ksp in RoomPlugin.kt

--- gradle_plugin/src/main/kotlin/RoomPlugin.kt
+++ gradle_plugin/src/main/kotlin/RoomPlugin.kt
@@ -1,13 +1,34 @@
 import ara.getVersionCatalogLibs
+import com.google.devtools.ksp.gradle.KspExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.process.CommandLineArgumentProvider
+import java.io.File
 
 class RoomPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
                 apply("com.google.devtools.ksp")
+            }
+
+            extensions.configure<KspExtension> {
+                class RoomSchemaArgProvider(
+                    @get:InputDirectory
+                    @get:PathSensitive(PathSensitivity.RELATIVE)
+                    val schemaDir: File
+                ) : CommandLineArgumentProvider {
+                    override fun asArguments(): Iterable<String> {
+                        return listOf("room.schemaLocation=${schemaDir.path}")
+                    }
+                }
+
+                arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
             }
 
             val libs = getVersionCatalogLibs()
