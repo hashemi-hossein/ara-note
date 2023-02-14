c391a19639d94a6b492eccb93ae26c5119f6df8d
Hossein Hashemi <hashemi@tutamail.com>
2023-02-14 09:47:00 +0330

    add :core:backup module

---
+++ core/backup/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("ara.library")
+    id("ara.hilt")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+}
+
+android {
+    namespace = "aranote.core.backup"
+}
+
+dependencies {
+
+    implementation(project(":core:entity"))
+    implementation(project(":core:data"))
+    implementation(project(":core:util"))
+
+    implementation(libs.javax.inject)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Kotlinx Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    testImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+}
