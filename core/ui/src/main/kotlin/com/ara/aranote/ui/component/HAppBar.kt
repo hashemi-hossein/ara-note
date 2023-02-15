31b0988c6d5cdf8eca639a7e3e995308eb3cd935
Hossein Hashemi <hashemi@tutamail.com>
2023-02-15 11:35:07 +0330

    move HAppBar.kt, HDropdown.kt and HSnackBar.kt to :core:ui

--- core/ui/src/main/kotlin/com/ara/aranote/ui/component/HAppBar.kt
+++ core/ui/src/main/kotlin/com/ara/aranote/ui/component/HAppBar.kt
@@ -14,7 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import com.ara.aranote.R
+import aranote.core.ui.R
 
 enum class AppBarNavButtonType {
     MENU, BACK
--- core/ui/src/main/kotlin/com/ara/aranote/ui/component/HDropdown.kt
+++ core/ui/src/main/kotlin/com/ara/aranote/ui/component/HDropdown.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.ara.aranote.R
+import aranote.core.ui.R
 
 @Composable
 fun HDropdown(
