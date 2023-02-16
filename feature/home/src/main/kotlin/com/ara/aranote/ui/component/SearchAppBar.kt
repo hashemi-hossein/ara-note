2f79e57208b45f1b00e623913884ffa97a6b5c36
Hossein Hashemi <hashemi@tutamail.com>
2023-02-16 14:48:53 +0330

    add SearchAppBar.kt

---
+++ feature/home/src/main/kotlin/com/ara/aranote/ui/component/SearchAppBar.kt
@@ -0,0 +1,70 @@
+package com.ara.aranote.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchAppBar(
+    searchText: String,
+    modifySearchText: (String?) -> Unit,
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            BasicTextField(
+                value = searchText,
+                onValueChange = { modifySearchText(it) },
+                Modifier.fillMaxWidth(),
+                decorationBox = @Composable { innerTextField ->
+                    TextFieldDefaults.TextFieldDecorationBox(
+                        value = searchText,
+                        innerTextField = innerTextField,
+                        enabled = true,
+                        singleLine = true,
+                        visualTransformation = VisualTransformation.None,
+                        interactionSource = remember { MutableInteractionSource() },
+                        trailingIcon = {
+                            IconButton(onClick = { modifySearchText(null) }) {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "close search"
+                                )
+                            }
+                        },
+                        container = {
+                            Box(
+                                Modifier.background(
+                                    color = MaterialTheme.colorScheme.surfaceVariant,
+                                    shape = CircleShape
+                                )
+                            )
+                        }
+                    )
+                }
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun HPreview() {
+    SearchAppBar(searchText = "search text", modifySearchText = {})
+}
