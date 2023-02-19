9253f934337e9f23a7d4c1da1b342600998c4862
Hossein Hashemi <hashemi@tutamail.com>
2023-02-19 14:02:38 +0330

    add observing notebooks with note count to NotebookDao.kt

--- core/database/src/main/kotlin/ara/note/data/localdatasource/NotebookDao.kt
+++ core/database/src/main/kotlin/ara/note/data/localdatasource/NotebookDao.kt
@@ -3,6 +3,7 @@ package ara.note.data.localdatasource
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.MapInfo
 import androidx.room.Query
 import androidx.room.Update
 import ara.note.data.model.NotebookModel
@@ -13,6 +14,15 @@ interface NotebookDao {
 
     @Query("SELECT * FROM tblNotebooks ORDER BY id ASC")
     fun observe(): Flow<List<NotebookModel>>
+
+    @MapInfo(keyTable = "tblNotebooks", valueColumn = "count")
+    @Query("""
+        SELECT tblNotebooks.id, tblNotebooks.name, COUNT(notebook_notes.notebook_id) AS count FROM tblNotebooks 
+        LEFT JOIN (SELECT notebook_id FROM tblNotes) AS notebook_notes ON tblNotebooks.id = notebook_notes.notebook_id 
+        GROUP BY tblNotebooks.id, tblNotebooks.name
+    """
+    )
+    fun observeWithCount(): Flow<Map<NotebookModel,Int>>
 
     @Insert
     suspend fun insert(notebookModel: NotebookModel): Long?
