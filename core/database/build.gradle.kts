39583d9c9bb6b1094ea96efdaeda727f858c6d89
Hossein Hashemi <hashemi@tutamail.com>
2023-02-28 10:33:33 +0330

    move TestDatabaseModule.kt

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -9,21 +9,9 @@ android {
 }
 
 dependencies {
-
     implementation(project(":core:entity"))
-    androidTestImplementation(project(":core:test"))
 
     implementation(libs.kotlinx.datetime)
 
-    // Kotlinx Coroutines
-    implementation(libs.kotlinx.coroutines.android)
-    testImplementation(libs.kotlinx.coroutines.test)
-    androidTestImplementation(libs.kotlinx.coroutines.test)
-
     testImplementation(libs.google.truth)
-    androidTestImplementation(libs.google.truth)
-    androidTestImplementation(libs.androidx.test.core.ktx)
-    androidTestImplementation(libs.androidx.test.rules)
-    androidTestImplementation(libs.androidx.test.ext.junit.ktx)
-    androidTestImplementation(libs.hilt.android.testing)
 }
--- core/test/build.gradle.kts
+++ core/test/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("ara.library")
+    id("ara.room")
 }
 
 android {
@@ -10,6 +11,7 @@ dependencies {
 
     implementation(project(":core:entity"))
     implementation(project(":core:util"))
+    implementation(project(":core:database"))
 
     implementation(libs.kotlinx.coroutines.test)
     implementation(libs.junit)
